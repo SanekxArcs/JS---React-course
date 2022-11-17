3374f3227993e1a6c5400504dbdc5d635e612173
Oleksandr Dzisiak <shumahersasha007@gmail.com>
2022-11-17 16:40:07 +0100

    add timer

--- Section 3/Food_dist/script.js
+++ Section 3/Food_dist/script.js
@@ -2,6 +2,29 @@
 
 window.addEventListener('DOMContentLoaded', () => {
 
+    const deadLine = '2022-11-17';
+
+    function getTimeRemaining(endtime) {
+        const t = Date.parse(endtime) - Date.parse(new Date()),
+        days = Math.floor(t / (1000 * 60 * 60 * 24)),
+        hours = Math.floor((t / 1000 * 60 * 60) % 24),
+        minutes = Math.floor((t / 1000 / 60) % 60),
+        seconds = Math.floor((t / 1000) % 60);
+
+        return {
+            'total': t,
+            'days': days,
+            'hours': hours,
+            'minutes': minutes,
+            'seconds': seconds
+        };
+    }
+
+    function setClock(selector, endtime) {
+        const timer = document.querySelector(selector),
+            
+    }
+
     let tabs = document.querySelectorAll('.tabheader__item'),
         tabsContent = document.querySelectorAll(".tabcontent"),
         tabsParent = document.querySelector('tabheader__item');
